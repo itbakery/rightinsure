467609136c532fe71e54732bc903617a043fec37
lookmee <lookmee@localhost.localdomain>
2014-01-31 10:44:09 +0700

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rightinsure::Application.config.secret_key_base = '1d29b315ca89ddcafe8e853d93c1b27e1a18c4faa5417e280435a7db074cad7ecafcd07fb6991abbbd987f3d68898be84fdb01f13c7085352b23439f19ef1606'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rightinsure::Application.config.session_store :cookie_store, key: '_rightinsure_session'
